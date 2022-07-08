cc71cee53071817c1f624ecb1e13b3152f1dbba2
Alkaidcc <932214806@qq.com>
2022-07-08 15:56:33 +0800

    feat: logic computation

--- script/index.js
+++ script/index.js
@@ -4,13 +4,13 @@ const display_top = document.querySelector('.top-display');
 const display_bottom = document.querySelector('.bottom-display');
 const key_clear = document.querySelector('#key-ac')
 const key_equal = document.querySelector('#key-equal')
-const key_divide = document.querySelector('#key-div')
-const key_multiply = document.querySelector('#key-mul')
-const key_subtract = document.querySelector('#key-sub')
-const key_add = document.querySelector('#key-add')
 
 operands.forEach(operand => {
   operand.addEventListener('click', function () {
+    if (display_top.innerHTML.indexOf('=') !== -1) {
+      display_top.innerHTML = '';
+      display_bottom.innerHTML = '';
+    }
     if (['+', '-', '×', '/'].includes(display_bottom.innerHTML)) {
       display_bottom.innerHTML = this.innerHTML;
     } else {
@@ -19,8 +19,13 @@ operands.forEach(operand => {
     display_top.innerHTML += this.innerHTML;
   });
 })
+
 operators.forEach(operator => {
   operator.addEventListener('click', function () {
+    console.log(display_top.innerHTML);
+    if (display_top.innerHTML.indexOf('=') !== -1) {
+      display_top.innerHTML = display_top.innerHTML.substring(display_top.innerHTML.indexOf('=') + 1, display_top.innerHTML.length);
+    }
     if (['+', '-', '×', '/'].includes(display_top.innerHTML.substring(display_top.innerHTML.length - 1))) {
       display_top.innerHTML = display_top.innerHTML.replace(/[\+\-×/]/g, this.innerHTML);
     } else {
@@ -29,7 +34,17 @@ operators.forEach(operator => {
     display_bottom.innerHTML = this.innerHTML;
   });
 })
+
 key_clear.addEventListener('click', function () {
   display_top.innerHTML = '';
   display_bottom.innerHTML = '';
+})
+
+key_equal.addEventListener('click', function () {
+  if (display_top.innerHTML.indexOf('=') == -1) {
+    let statement = display_top.innerHTML.replace('×', '*');
+    let result = eval(statement);
+    display_top.innerHTML += '=' + result;
+    display_bottom.innerHTML = result;
+  }
 })
